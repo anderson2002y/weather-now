729577cf89441ed93d32cd81d7d87cff4af90c75
Anderson Sánchez <anderson2002y@gmail.com>
2025-05-17 18:04:23 -0600

    First commit

---
+++ src/components/Form/Form.tsx
@@ -0,0 +1,41 @@
+import { countries } from "../../data/countries";
+import styles from './Form.module.css';
+
+export default function Form() {
+  return (
+    <form className={styles.form}>
+
+      <div className={styles.field}>
+        <label htmlFor="city">Ciudad:</label>
+        <input 
+          id="city"
+          type="text"
+          name="city"
+          placeholder="Ciudad"
+        />
+      </div>
+
+      <div className={styles.field}>
+        <label htmlFor="pais">Pais:</label>
+        <select id="pais">
+          <option value="">-- Seleccione --</option>
+
+          {countries.map( country => (
+            <option
+              key={country.code}
+              value={country.code}
+            > {country.name}
+            </option>
+          ))}
+        </select>
+      </div>
+
+      <input
+        className={styles.submit}
+        type="submit"
+        value='Consultar Clima'
+      />
+
+    </form>
+  )
+}
---
+++ src/data/countries.ts
@@ -0,0 +1,9 @@
+export const countries = [
+  { code: "NI", name: "Nicaragua" },
+  { code: "US", name: "Estados Unidos" },
+  { code: "MX", name: "México" },
+  { code: "AR", name: "Argentina" },
+  { code: "CO", name: "Colombia" },
+  { code: "CR", name: "Costa Rica" },
+  { code: "ES", name: "España" },
+];
