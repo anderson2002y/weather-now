6e24bde8e4671048353fcca67cf4d3f3a0afa998
Anderson Sánchez <anderson2002y@gmail.com>
2025-05-18 10:02:45 -0600

    feat: add loading spinner and display message when city not found

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import styles from './App.module.css';
+import Alert from './components/Alert/Alert';
 import Form from './components/Form/Form';
+import Spinner from './components/Spinner/Spinner';
 import WeatherDetail from './components/WeatherDetail/WeatherDetail';
 import useWeather from './hooks/useWeather';
 
 function App() {
 
-  const { weather, fetchWeather, hasWeatherData } = useWeather();
+  const { weather, loading, notFound, fetchWeather, hasWeatherData } = useWeather();
 
   return (
     <>
@@ -16,11 +18,9 @@ function App() {
           fetchWeather= {fetchWeather}
         />
 
-        {hasWeatherData &&
-          <WeatherDetail
-            weather={weather}
-          />
-        }
+        {loading && <Spinner/>}
+        {hasWeatherData && <WeatherDetail weather={weather} /> }
+        {notFound && <Alert>Ciudad no Encontrada</Alert>}
       </div>
     </>
   )
--- src/hooks/useWeather.ts
+++ src/hooks/useWeather.ts
@@ -14,30 +14,40 @@ const Weather = z.object({
 })
 
 const initialState = {
-    name: '',
-    main: {
-      temp: 0,
-      temp_max: 0,
-      temp_min: 0
-    }
+  name: '',
+  main: {
+    temp: 0,
+    temp_max: 0,
+    temp_min: 0
+  }
 }
 
 export type Weather = z.infer<typeof Weather>
 
 export default function useWeather() {
 
-const [weather, setWeather] = useState<Weather>(initialState);
+  const [weather, setWeather] = useState<Weather>(initialState);
+  const [loading, setLoading] = useState(false);
+  const [notFound, setNotFound] = useState(false);
 
   const fetchWeather = async (search: SearchType) => {
+
+    setLoading(true);
+    setWeather(initialState);
+
     try {
       const appId = import.meta.env.VITE_API_KEY;
       const geoUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${search.city},${search.country}&appid=${appId}`;
-
       const {data} = await axios.get(geoUrl);
+
+      // Comprobar si Existe
+      if(!data[0]) {
+        setNotFound(true);
+        return;
+      }
 
       const lat = data[0].lat;
       const lon = data[0].lon;
-
       const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${appId}`;
       
       const {data: weatherResult} = await axios(weatherUrl);
@@ -49,6 +59,8 @@ const [weather, setWeather] = useState<Weather>(initialState);
 
     } catch (error) {
       console.log(error);
+    } finally {
+      setLoading(false);
     }
   };
 
@@ -56,6 +68,8 @@ const [weather, setWeather] = useState<Weather>(initialState);
 
   return {
     weather,
+    loading,
+    notFound,
     fetchWeather,
     hasWeatherData
   };
